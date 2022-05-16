8a7f0cba7c7eecea0a224dbb611b07ad32e2395b
solome <solome@outlook.com>
2022-05-16 21:07:19 +0800

    fix:修复 `.mp4` 跨域问题

--- lib/Player/modules/VideoAgent/VideoAgentMesh.d.ts
+++ lib/Player/modules/VideoAgent/VideoAgentMesh.d.ts
@@ -10,9 +10,9 @@ export interface VideoAgentMeshOptions {
      */
     audioInstance?: HTMLAudioElement;
     /**
-     * 是否开启音视频预载能力。
+     * 是否开启音频预载能力。**仅对`.mp3`有效**。
      *
-     * @description 开启预载能力后会通过 `Fetch/XHR` 方式将音视频文件下载下载转成二进制 [`Blob`](https://developer.mozilla.org/zh-CN/docs/Web/API/Blob) 提供给多媒体实例。这样的好处是 **能够保障音视频播放过程中不卡顿**。 但在部分 iOS 系统中会存在兼容问题——播 25s 之后会静音（`muted` 为 `false`，但是没有声音）。
+     * @description 开启预载能力后会通过 `Fetch/XHR` 方式将音频文件下载转成二进制 [`Blob`](https://developer.mozilla.org/zh-CN/docs/Web/API/Blob) 提供给多媒体实例。这样的好处是 **能够保障音视频播放过程中不卡顿**。 但在部分 iOS 系统中会存在兼容问题——播 25s 之后会静音（`muted` 为 `false`，但是没有声音）。
      */
     preload?: boolean;
 }
