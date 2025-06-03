3419c7fa098610b3b44d152bfa4bcdca6a046840
lichengjie003 <lichengjie003@realsee.com>
2025-06-03 20:29:18 +0800

    fix: 解决文档生成中的所有警告

--- typedoc/Player.ts
+++ typedoc/Player.ts
@@ -2,7 +2,14 @@ export type { Player } from '../resources/Player'
 export type { Controller } from '../resources/Player/Controller'
 export type { VideoAgentScene } from '../resources/Player/modules/VideoAgent/VideoAgentScene'
 export type { VideoAgentMesh, VideoAgentMeshOptions } from '../resources/Player/modules/VideoAgent/VideoAgentMesh'
-export type { PlayerConfigs, CustomVreoKeyframeProps, VreoSubscribe} from '../resources/Player/typings'
+export type { 
+  PlayerConfigs, 
+  CustomVreoKeyframeProps, 
+  VreoSubscribe,
+  Appearance,
+  WaveAppearance,
+  AppSize
+} from '../resources/Player/typings'
 export type {
   VreoKeyframeEnum, VreoKeyframe, VreoVideo, VreoUnit,
   VreoKeyframeConfigMap as VreoKeyframeMap, VreoKeyframeEvent, CameraMovementData,
